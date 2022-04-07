c4fb81e29aca9e8c12592fcf4d697b8db9adbf2c
dtran556 <76986117+dtran556@users.noreply.github.com>
2022-04-06 19:27:47 -0700

    Update Graph.java

--- Lab 6 - Graphs/Graph.java
+++ Lab 6 - Graphs/Graph.java
@@ -36,7 +36,6 @@ public class Graph {
 		parent = new ArrayList<Integer>(n);
 		discoverTime = new ArrayList<>(n);
 		finishTime = new ArrayList<>(n);
-		
 
 		for (int i = 0; i < n + 1; i++) {
 			color.add('W');
@@ -86,7 +85,7 @@ public class Graph {
 	 * @throws IndexOutOfBoundsException when v is out of bounds
 	 */
 	public Integer getDistance(Integer v) throws IndexOutOfBoundsException {
-		if (v < 0 && v > vertices) {
+		if (v <= 0 || v > vertices) {
 			throw new IndexOutOfBoundsException("getDistance(): vertex is not on graph");
 		}
 		return distance.get(v);
@@ -101,7 +100,7 @@ public class Graph {
 	 * @throws IndexOutOfBoundsException when v is out of bounds
 	 */
 	public Integer getParent(Integer v) throws IndexOutOfBoundsException {
-		if (v < 0 && v > vertices) {
+		if (v <= 0 || v > vertices) {
 			throw new IndexOutOfBoundsException("getParent(): vertex is not on graph");
 		}
 		return parent.get(v);
@@ -116,11 +115,11 @@ public class Graph {
 	 * @throws IndexOutOfBoundsException when v is out of bounds
 	 */
 	public Character getColor(Integer v) throws IndexOutOfBoundsException {
-		if (v < 0 && v > vertices) {
+		if (v <= 0 || v > vertices) {
 			throw new IndexOutOfBoundsException("getColor(): vertex is not on graph");
 		}
 		return color.get(v);
-	
+
 	}
 
 	/**
@@ -132,7 +131,7 @@ public class Graph {
 	 * @throws IndexOutOfBoundsException when v is out of bounds
 	 */
 	public Integer getDiscoverTime(Integer v) throws IndexOutOfBoundsException {
-		if (v < 0 && v > vertices) {
+		if (v <= 0 || v > vertices) {
 			throw new IndexOutOfBoundsException("getDistance(): vertex is not on graph");
 		}
 		return discoverTime.get(v);
@@ -147,7 +146,7 @@ public class Graph {
 	 * @throws IndexOutOfBoundsException when v is out of bounds
 	 */
 	public Integer getFinishTime(Integer v) throws IndexOutOfBoundsException {
-		if (v < 0 && v > vertices) {
+		if (v <= 0 || v > vertices) {
 			throw new IndexOutOfBoundsException("getDistance(): vertex is not on graph");
 		}
 		return finishTime.get(v);
@@ -162,7 +161,7 @@ public class Graph {
 	 * @throws IndexOutOfBoundsException when v is out of bounds
 	 */
 	public LinkedList<Integer> getAdjacencyList(Integer v) throws IndexOutOfBoundsException {
-		if (v < 0 && v > vertices) {
+		if (v <= 0 || v > vertices) {
 			throw new IndexOutOfBoundsException("getDistance(): vertex is not on graph");
 		}
 		return adj.get(v);
@@ -172,30 +171,29 @@ public class Graph {
 
 	/**
 	 * Inserts vertex v into the adjacency list of vertex u (i.e. inserts v into the
-	 * list at index u)
-	 *  @precondition, 0 < u, v <= vertices
+	 * list at index u) @precondition, 0 > u & v, u & v <= vertices
 	 * 
 	 * @throws IndexOutOfBounds exception when u or v is out of bounds
 	 */
 	public void addDirectedEdge(Integer u, Integer v) throws IndexOutOfBoundsException {
 		if (!(u > 0 && u <= vertices && v > 0 && v <= vertices)) {
-		//if(v.compareTo(v))
+			// if(v.compareTo(v))
 			throw new IndexOutOfBoundsException("addDirectedEdge(): vertex is not on graph ");
 		}
-		
+
 		adj.get(u).addLast(v);
 		edges++;
 	}
 
 	/**
 	 * Inserts vertex v into the adjacency list of vertex u (i.e. inserts v into the
-	 * list at index u) and inserts u into the adjacent vertex list of v 
-	 * @precondition, 0 < u, v <= vertices
+	 * list at index u) and inserts u into the adjacent vertex list of
+	 * v @precondition, 0 > u & v, u & v <= vertices
 	 * 
 	 * @throws IndexOutOfBoundsException when u or v is out of bounds
 	 */
 	public void addUndirectedEdge(Integer u, Integer v) throws IndexOutOfBoundsException {
-		if (!(u > 0 && u <= vertices && v > 0 && v <= vertices))  {
+		if (!(u > 0 && u <= vertices && v > 0 && v <= vertices)) {
 			throw new IndexOutOfBoundsException("addUndirectedEdge(): vertex is not on graph ");
 		}
 		adj.get(u).addLast(v);
@@ -231,52 +229,38 @@ public class Graph {
 		if (source <= 0 || source > vertices) {
 			throw new IndexOutOfBoundsException("BFS(): vertex is outside the bounds of the graph");
 		}
-		for (int i = 1; i <= vertices; i++) {
-			color.set(i, 'W');
-			distance.set(i, -1);
-			parent.set(i, null);
-		}
-		
 		color.set(source, 'G');
 		distance.set(source, 0);
 		LinkedList<Integer> Q = new LinkedList<Integer>();
-		
 		Q.addLast(source);
-
 		while (!Q.isEmpty()) {
 			int x = Q.getFirst();
 			Q.removeFirst();
-
 			adj.get(x).positionIterator();
-			for (int i = 1; i < adj.get(x).getLength(); i++) {
+			while (!adj.get(x).offEnd()) {
 				int y = adj.get(x).getIterator();
 				if (color.get(y) == 'W') {
 					color.set(y, 'G');
 					distance.set(y, getDistance(x) + 1);
 					parent.set(y, x);
 					Q.addLast(y);
-				
+
 				}
 				adj.get(x).advanceIterator();
 			}
-			//System.out.println(distance);
 			color.set(x, 'B');
 		}
+
 	}
 
 	/**
 	 * Performs depth first search on this Graph in order of vertex lists
 	 */
 	public void DFS() {
-		/*for (int i = 1; i <= vertices; i++) {
-			color.add(i, 'w');
-			parent.add(i, null);
-			discoverTime.add(i, -1);
-			finishTime.add(i, -1);
-		}*/
 		time = 0;
 		for (int i = 1; i <= vertices; i++) {
 			if (color.get(i) == 'W') {
+				// System.out.println("start)");
 				visit(i);
 			}
 		}
@@ -288,22 +272,24 @@ public class Graph {
 	 * @param vertex the vertex to visit
 	 */
 	private void visit(int vertex) {
-		color.add(vertex, 'G');
+		color.set(vertex, 'G');
 		time++;
-		discoverTime.set(vertex,time);
-		
-		
-		for (int i = 1; i < adj.size(); i++) {
-			if (color.get(i) == 'W') {
-				parent.set(i, vertex);
-				visit(i);
+		discoverTime.set(vertex, time);
+		// System.out.println("visit");
+
+		adj.get(vertex).positionIterator();
+		// System.out.println(color.get(adj.get(vertex).getIterator()));
+		while (!adj.get(vertex).offEnd()) {
+			if (color.get(adj.get(vertex).getIterator()) == 'W') {
+				parent.set(adj.get(vertex).getIterator(), vertex);
+				visit(adj.get(vertex).getIterator());
 			}
+			adj.get(vertex).advanceIterator();
 		}
 		color.set(vertex, 'B');
 		time++;
-		finishTime.set(vertex,time);
-		
-		
+		finishTime.set(vertex, time);
+
 	}
 
 }
